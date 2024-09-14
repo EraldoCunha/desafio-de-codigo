836c9c8e9a34b4ce7f9cd3fd066c4a74fe83422e
Eraldo Cunha <eraldo_cunha@hotmail.com>
2024-09-13 21:43:16 -0300

    Desafio 01 respondido

--- src/desafio_01/Program.java
+++ src/desafio_01/Program.java
@@ -1,4 +1,40 @@
 package desafio_01;
 
+import java.util.Scanner;
+
 public class Program {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Informe um número: ");
+        int numero = sc.nextInt();
+
+        if (estaPresente(numero)) {
+            System.out.println("O número " + numero + " pertence à sequência de Fibonacci.");
+        } else {
+            System.out.println("O número " + numero + " não pertence à sequência de Fibonacci.");
+        }
+
+        sc.close();
+    }
+
+    public static boolean estaPresente(int numero) {
+        if (numero == 0 || numero == 1) {
+            return true;
+        }
+
+        int anterior = 0;
+        int atual = 1;
+        int proximo;
+
+        while (atual < numero) {
+            proximo = anterior + atual;
+            anterior = atual;
+            atual = proximo;
+        }
+
+        return atual == numero;
+    }
 }
