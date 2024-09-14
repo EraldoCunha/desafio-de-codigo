1a90444aee5f8f3d63dc15cfebe6c3ccfcc90860
Eraldo Cunha <eraldo_cunha@hotmail.com>
2024-09-13 22:08:50 -0300

    Código do desafio 03 replicado

---
+++ src/desafio_03/Program.java
@@ -0,0 +1,18 @@
+package desafio_03;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        int indice = 12;
+        int soma = 0;
+        int k = 1;
+
+        while (k < indice) {
+            k = k + 1;
+            soma = soma + k;
+        }
+
+        System.out.println("O valor final de SOMA é: " + soma);
+    }
+}
